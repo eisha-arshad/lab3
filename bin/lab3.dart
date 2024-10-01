b65c72d88a9132fc4176d01820a9ac032479a3c4
eisha-arshad <159758891+eisha-arshad@users.noreply.github.com>
2024-10-01 10:18:28 +0500

    new lab

---
+++ bin/lab3.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  List<String> maplist =['Eisha','Laiba','Bisma'];
+  String? firstelement=maplist[0];
+  String? secodelement=maplist[1];
+  String? thirdelement=maplist[2];
+maplist.add('Esh');
+print(maplist);
+print(firstelement);
+print(secodelement);
+print(thirdelement);
+}
+
+
